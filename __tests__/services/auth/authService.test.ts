208d23d7ae570e828554127822926818338feeb2
Kingestif <kingestiff@gmail.com>
2025-07-17 15:57:59 +0300

    refactor: remove password from user object in authService test save method since we removed the DTO previous commit

--- __tests__/services/auth/authService.test.ts
+++ __tests__/services/auth/authService.test.ts
@@ -42,7 +42,6 @@ describe("authService", () => {
                 save: async (user: Usertype) => {
                     return {
                         email: user.email,
-                        password: user.password,
                         role: user.role
                     }
                 }
