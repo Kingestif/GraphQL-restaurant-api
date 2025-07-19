7c24d5981cc6fdb4fa0f5cac4426a3db09f3f156
Kingestif <kingestiff@gmail.com>
2025-07-19 11:44:14 +0300

    fix: ensure id field is validated as required in menuValidation since we need it on update

--- validation/menuValidation.ts
+++ validation/menuValidation.ts
@@ -1,6 +1,7 @@
 import { z } from 'zod';
 
 export const menuValidation = z.object({
+    id: z.string().min(1, 'id is a required field'),
     name: z.string().min(1, 'name is required'),
     description: z.string().min(1, 'description is required'),
     price: z.number().min(1, 'number is required'),
